886fbacc2a03b7ff10a54e7048111e2571080383
Nkwachi <nkwachinwamaghinna@gmail.com>
2024-07-10 10:55:56 +0100

    transfer ui

---
+++ lib/src/ui/views/transfer/view_bank_details_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:spotflow/gen/assets.gen.dart';
+import 'package:spotflow/src/ui/widgets/base_scaffold.dart';
+import 'package:spotflow/src/ui/widgets/cancel_payment_button.dart';
+import 'package:spotflow/src/ui/widgets/change_payment_button.dart';
+import 'package:spotflow/src/ui/widgets/payment_card.dart';
+import 'package:spotflow/src/ui/widgets/payment_options_tile.dart';
+import 'package:spotflow/src/ui/widgets/pci_dss_icon.dart';
+
+class ViewBankDetailsPage extends StatelessWidget {
+  const ViewBankDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScaffold(children: [
+      PaymentOptionsTile(
+        icon: Assets.svg.payWithTransferIcon.svg(),
+        text: 'Pay with transfer',
+      ),
+      const PaymentCard(),
+      const Spacer(),
+      const Row(
+        children: [
+          Expanded(
+            child: ChangePaymentButton(),
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Expanded(
+            child: CancelPaymentButton(),
+          ),
+        ],
+      ),
+      const Spacer(),
+      const PciDssIcon(),
+      const SizedBox(
+        height: 32,
+      )
+    ]);
+  }
+}
